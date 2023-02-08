eb2f383d209651e54a5ecefbd1d290fcdcb4dcbf
Jean-Paul <hawa029@outlook.fr*>
2023-02-08 15:48:58 +0100

    clean folders

--- project/views.py
+++ project/views.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
-from sklearn.tree import DecisionTreeClassifier
 import numpy as np
 import pandas as pd
-
 
 
 app = Flask(__name__)
@@ -19,27 +17,9 @@ def index():
 def register():
     return render_template('auth/register.html')
 
-# import d'un petit fichier de users
-def valid_login():
-    with open('users.txt', 'r') as users:
-        for i, j in users:
-            users[i]
-            users[j]
-
-def log_user_in():
-    return render_template('auth/dashboard.html')
 
 @app.route('/auth/login', methods=['POST', 'GET'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if valid_login(request.form['username'],
-                       request.form['password']):
-            return log_user_in(request.form['username'])
-        else:
-            error = 'Invalid username/password'
-    # le code suivant est exécuté si la méthode 
-    # fut GET ou les identifiants étaient incorrectes
     return render_template('auth/login.html')
 
 @app.route('/auth/dashboard')
@@ -62,7 +42,8 @@ def predict_post():
     #feature3 = (request.form["feature3"])
 
     # Prédiction
-    prediction = model.predict(np.array([[feature1, 0]]))
+    x = np.array([[feature1], [0]])
+    prediction = model.predict(x)
 
     return render_template('auth/prediction.html', prediction=prediction)
 
